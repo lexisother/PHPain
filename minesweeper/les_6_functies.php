07c7e1e3c0561ce12aa9db7a4dde12170147de1d
Alyxia Sother <alyxia@riseup.net>
2022-04-14 13:44:20 +0200

    [minesweeper] Add number checking for mines

--- minesweeper/les_6_functies.php
+++ minesweeper/les_6_functies.php
@@ -1,24 +1,28 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if (isset($_SESSION['game'])) {
   $board = $_SESSION['game'];
 } else {
   $board = genEmptyBoard();
   $board = placeBomb($board, 10);
+  $board = checkBombs($board);
 }
 
 function showBoard($board)
 {
-  echo '<table>';
-  for ($i = 0; $i < 7; $i++) {
-    echo '<tr>';
-    for ($j = 0; $j < 10; $j++) {
-      echo '<td>';
-      echo $board[$i][$j];
-      echo '</td>';
+  echo "<table>";
+  foreach ($board as $rows) {
+    echo "<tr>";
+    foreach ($rows as $row) {
+      echo "<td>";
+      echo $row;
+      echo "</td>";
     }
-    echo '</tr>';
+    echo "</tr>";
   }
-  echo '</table>';
+  echo "</table>";
 }
 
 function placeBomb($board, $aantal)
@@ -44,8 +48,71 @@ function genEmptyBoard()
   for ($i = 0; $i < 7; $i++) {
     $board[$i] = [];
     for ($j = 0; $j < 10; $j++) {
-      $board[$i][$j] = "";
+      $board[$i][$j] = 0;
     }
   }
+  return $board;
+}
+
+function checkBombs($board)
+{
+  for ($x = 0; $x < count($board); $x++) {
+    for ($y = 0; $y < count($board[0]); $y++) {
+      if ($board[$x][$y] === "\x58") {
+        // Left {{{
+        if ($y - 1 >= 0) {
+          if ($board[$x][$y - 1] !== "\x58") {
+            $board[$x][$y - 1] += 1;
+          }
+
+          if ($x - 1 >= 0) {
+            if ($board[$x - 1][$y - 1] !== "\x58") {
+              $board[$x - 1][$y - 1] += 1;
+            }
+          }
+
+          if ($x + 1 < count($board)) {
+            if ($board[$x + 1][$y - 1] !== "\x58") {
+              $board[$x + 1][$y - 1] += 1;
+            }
+          }
+        }
+        // }}}
+
+        // Right {{{
+        if ($y + 1 < count($board[0])) {
+          if ($board[$x][$y + 1] !== "\x58") {
+            $board[$x][$y + 1] += 1;
+          }
+          if ($x - 1 >= 0) {
+            if ($board[$x - 1][$y + 1] !== "\x58") {
+              $board[$x - 1][$y + 1] += 1;
+            }
+          }
+          if ($x + 1 < count($board)) {
+            if ($board[$x + 1][$y + 1] !== "\x58") {
+              $board[$x + 1][$y + 1] += 1;
+            }
+          }
+        }
+        // }}}
+
+        // Above
+        if ($x - 1 >= 0) {
+          if ($board[$x - 1][$y] !== "\x58") {
+            $board[$x - 1][$y] += 1;
+          }
+        }
+
+        // Under
+        if ($x + 1 < count($board)) {
+          if ($board[$x + 1][$y] !== "\x58") {
+            $board[$x + 1][$y] += 1;
+          }
+        }
+      }
+    }
+  }
+
   return $board;
 }
