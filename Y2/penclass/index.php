f0822c6dd8f028b15547c0c8fcf02aba795bfb21
Alyxia Sother <alyxia@riseup.net>
2022-09-12 18:18:07 +0000

    [Y2/penclass] Add Niels code

--- Y2/penclass/index.php
+++ Y2/penclass/index.php
@@ -2,42 +2,65 @@
 
 ini_set('display_errors', 1);
 
+/**
+ * Pen that writes.
+ * @author Alyxia
+ */
 class Pen
 {
   public $kleur;
   public $dop;
-  // private $inhoud;
 
-  public function __construct(string $kleur, bool $dop)
+  private $inhoud;
+
+  /**
+   * Creates a new pen instance.
+   *
+   * @param string $kleur
+   * @return void
+   */
+  public function __construct(string $kleur)
   {
     $this->kleur = $kleur;
-    $this->dop = $dop;
+    $this->dop = false;
+    $this->inhoud = 100;
   }
 
-  public function vliegen($var)
+  private function vloeien($inkt)
   {
-    // NOOP
-  }
-
-  private function vloeien()
-  {
-    // NOOP
+    $aantal = strlen($inkt);
+    if ($this->inhoud > $aantal) {
+      $this->inhoud -= $aantal;
+      echo $inkt;
+    }
   }
 
   public function schrijf($iets)
   {
-    // Make sure a pen with a cap fails to write.
     if ($this->dop) {
-      throw new Exception("I can't write, there is a cap on me!");
+      $output = "<p>{$iets}</p>";
+      $this->vloeien($output);
     }
-    echo "<p><span style='color: {$this->kleur};'>{$iets}</span></p>";
+    // // Make sure a pen with a cap fails to write.
+    // if ($this->dop) {
+    //   throw new Exception("I can't write, there is a cap on me!");
+    // }
+    // echo "<p><span style='color: {$this->kleur};'>{$iets}</span></p>";
   }
 }
 
-$rodePen = new Pen("#ff0000", false);
-$groenePen = new Pen("#00ff00", false);
-$blauwePen = new Pen("#0000ff", false);
+// $rodePen = new Pen("#ff0000", false);
+// $groenePen = new Pen("#00ff00", false);
+// $blauwePen = new Pen("#0000ff", false);
+//
+// $rodePen->schrijf("hoi rood");
+// $groenePen->schrijf("hoi groen");
+// $blauwePen->schrijf("hoi blauw");
 
-$rodePen->schrijf("hoi rood");
-$groenePen->schrijf("hoi groen");
-$blauwePen->schrijf("hoi blauw");
+$markerA = new Pen("#ff0000");
+$markerB = new Pen("#00ff00");
+$markerA->dop = true;
+$markerB->dop = true;
+
+$markerA->schrijf("hoi dsajdksajkdhashdsakjdhskdhkasjdhsahjkdsa adjkshdsajkdhsadhkajshdashdsjkjkdahsjkdhaskjhsakjdhsahdkjas");
+$markerB->schrijf("hoi");
