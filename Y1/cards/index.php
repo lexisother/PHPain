2e06eec83d09562306f602b216316f7b987c73ae
Alyxia Sother <alyxia@riseup.net>
2022-09-12 07:36:37 +0000

    [Meta] Cleanup old files into subdir

---
+++ Y1/cards/index.php
@@ -0,0 +1,42 @@
+<html lang="nl" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Kaartendeck</title>
+    <meta name="author" content="Alyxia Sother" />
+    <link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <kaart>
+      <div>2<br/>&#9830;</div>
+      <div>2<br/>&#9830;</div>
+
+      <tekens class='T2'>
+        <icon><p>&#9830;</p></icon>
+        <icon><p>&#9830;</p></icon>
+      </tekens>
+    </kaart>
+
+    <kaart>
+      <div>3<br/>&#9830;</div>
+      <div>3<br/>&#9830;</div>
+
+      <tekens class='T3'>
+        <icon><p>&#9830;</p></icon>
+        <icon><p>&#9830;</p></icon>
+        <icon><p>&#9830;</p></icon>
+      </tekens>
+    </kaart>
+
+    <kaart>
+      <div>4<br/>&#9830;</div>
+      <div>4<br/>&#9830;</div>
+
+      <tekens class='T4'>
+        <icon><p>&#9830;</p></icon>
+        <icon><p>&#9830;</p></icon>
+        <icon><p>&#9830;</p></icon>
+        <icon><p>&#9830;</p></icon>
+      </tekens>
+    </kaart>
+  </body>
+</html>
